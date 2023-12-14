9ea493beeeaf04cba30a0b0e4774acec5957dce4
きくたたくや <gtk.9223@gmail.com>
2023-12-14 10:05:46 +0900

    とりあえずpug使える設定

--- gulpfile.js
+++ gulpfile.js
@@ -20,11 +20,51 @@ const rename = require("gulp-rename");
 const imagemin = require('gulp-imagemin');
 const mozjpeg = require('imagemin-mozjpeg');
 const pngquant = require("imagemin-pngquant");
+const pug = require('gulp-pug');
+const debug = require('gulp-debug');
+const filter = require('gulp-filter');
+const htmlbeautify = require('gulp-html-beautify');
 
 
 //----------------------------------------------------------------------
 //  各種関数設定
 //----------------------------------------------------------------------
+
+// html
+// const copyHtml = () => {
+//   return src("./src/html/**/*.html")
+//     .pipe(dest("./dist"))
+// }
+
+// pug
+const compilePug = () => {
+  return src("./src/html/**/*.pug", { sourcemaps: true })
+  .pipe(plumber({
+    errorHandler: notify.onError('Error: <%= error.message %>')
+  }))
+  .pipe(filter(function (file) { // _から始まるファイルを除外
+    return !/\/_/.test(file.path) && !/^_/.test(file.relative);
+  }))
+  .pipe(pug())
+  .pipe(htmlbeautify({
+    eol: '\n',
+    indent_size: 2,
+    indent_char: ' ',
+    indent_with_tabs: false,
+    end_with_newline: true,
+    preserve_newlines: true,
+    max_preserve_newlines: 2,
+    indent_inner_html: true,
+    brace_style: 'collapse',
+    indent_scripts: 'normal',
+    wrap_line_length: 0,
+    wrap_attributes: 'auto'
+  }))
+  .pipe(debug({title: 'pug dist:'}))
+  .pipe(dest("./dist",
+  // { sourcemaps: "." }
+  ))
+}
 
 // sass
 const compileSass = () => {
@@ -45,12 +85,6 @@ const jqueryMini = () => {
   return src("./node_modules/jquery/dist/jquery.min.js", { sourcemaps: true })
   .pipe(uglify())
   .pipe(dest("./dist/js/jquery/", { sourcemaps: "." }))
-}
-
-// html
-const copyHtml = () => {
-  return src("./src/html/**/*.html")
-    .pipe(dest("./dist"))
 }
 
 // icon
@@ -84,7 +118,7 @@ const copyImage = () => {
 const watchFiles = () => {
   watch("./src/sass/**/*.scss", { ignoreInitial: false }, compileSass).on("change", reload)
   watch("./src/js/*.js", { ignoreInitial: false }, minifyJs).on("change", reload)
-  watch("./src/html/**/*.html", { ignoreInitial: false }, copyHtml).on("change", reload)
+  watch("./src/html/**/*.pug", { ignoreInitial: false }, compilePug).on("change", reload)
 
   browserSync.init({
       server: {
@@ -97,6 +131,13 @@ const watchFiles = () => {
 //----------------------------------------------------------------------
 //  build時の設定
 //----------------------------------------------------------------------
+
+//Pugのコンパイル
+const buildPug = () => {
+  return src("./src/html/**/*.pug")
+    .pipe(pug({ outputStyle: "compressed" }))
+    .pipe(dest("./dist"))
+}
 
 //Sassのコンパイル
 const buildSass = () => {
@@ -135,7 +176,7 @@ const buildSlickThemeSass = () => {
 
 
 const deletFiles = async (cb) => {
-  const filesToDelete = ["./dist/css/style.css.map", "./dist/js/main.js.map", "./dist/js/sub.js.map"]
+  const filesToDelete = ["./dist/css/style.css.map", "./dist/js/main.js.map", "./dist/js/sub.js.map", "./dist/**/index.html.map"]
 
   let deletedCount = 0
 
@@ -157,4 +198,4 @@ const deletFiles = async (cb) => {
 // watch設定
 exports.dev = parallel(watchFiles, buildSass, buildJs, buildJquery)
 // build設定
-exports.build = parallel(buildSass, buildJs, deletFiles, buildJquery, icon, copyImage, buildSlickJs, buildSlickSass, buildSlickThemeSass)
+exports.build = parallel(buildSass, buildJs, deletFiles, buildJquery, icon, copyImage, buildSlickJs, buildSlickSass, buildSlickThemeSass, buildPug)
